8dae5eb07a61419ff1bf75f554d99c30223d8723
Diego I. Guardiola <diegoguardiola01@gmail.com>
2022-11-05 18:06:39 -0400

    Add files via upload

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,30 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicle").Vehicle
+
+
+
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage)
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 200;
+        this.fuel = 25;
+        this.scheduleService = false;
+    }
+    start() {
+        if (this.fuel > 0) {            
+            console.log("engine has started.");
+            return this.started = true
+        } else {
+            console.log("no fuel");
+            return this.started = false;
+        }
+    }
+}
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+let newCar = new Car("Mecury", "Sedan", "1965", "color", "mileage");
+newCar.start()
+console.log(newCar)
